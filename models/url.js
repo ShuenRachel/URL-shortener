53b71218a0c02c03ec2764819b991b54f14aac2b
ShuenRachel <shuen0410@gmail.com>
2021-11-04 19:17:54 +0800

    feat: add models

---
+++ models/url.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const UrlSchema = new Schema ({
+  original_url: {
+    type: String,
+    required: true
+  },
+  code: {
+    type: String
+  }
+})
+
+module.exports = mongoose.model('Url', UrlSchema)
